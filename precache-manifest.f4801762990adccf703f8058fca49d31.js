cf787a382c6ccf39dbb803713a1fbde3c0734ce1
shivam srivastava <shivamsrivastava033@gmail.com>
2021-07-24 11:09:56 +0530

    Updates

---
+++ precache-manifest.f4801762990adccf703f8058fca49d31.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e268838c07647c6ae4e7569c948cdf07",
+    "url": "/My-React-Website/index.html"
+  },
+  {
+    "revision": "e99501292f335cbe3095",
+    "url": "/My-React-Website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "aae1c80c01dc5ca5d737",
+    "url": "/My-React-Website/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "e99501292f335cbe3095",
+    "url": "/My-React-Website/static/js/2.49cf189c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/My-React-Website/static/js/2.49cf189c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae1c80c01dc5ca5d737",
+    "url": "/My-React-Website/static/js/main.541dc1da.chunk.js"
+  },
+  {
+    "revision": "6834d8c1321e737ed86d",
+    "url": "/My-React-Website/static/js/runtime-main.37e6a295.js"
+  },
+  {
+    "revision": "65f9ff46532d60827862a9239dc053cc",
+    "url": "/My-React-Website/static/media/Front.65f9ff46.jpg"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/My-React-Website/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/My-React-Website/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/My-React-Website/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/My-React-Website/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/My-React-Website/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/My-React-Website/static/media/software.43049cae.jpeg"
+  }
+]);
